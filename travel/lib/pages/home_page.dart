c5d9bcbe7d04cdaa6a5a3c83144c03c5e7effd50
wy521angel <693962802@qq.com>
2020-09-02 14:38:35 +0800

    首页模块完成

--- travel/lib/pages/home_page.dart
+++ travel/lib/pages/home_page.dart
@@ -6,8 +6,13 @@ import 'package:travel/dao/home_dao.dart';
 import 'package:travel/model/common_model.dart';
 import 'package:travel/model/grid_nav_model.dart';
 import 'package:travel/model/home_model.dart';
+import 'package:travel/model/sales_box_model.dart';
 import 'package:travel/widget/grid_nav.dart';
+import 'package:travel/widget/loading_container.dart';
 import 'package:travel/widget/local_nav.dart';
+import 'package:travel/widget/sales_box.dart';
+import 'package:travel/widget/sub_nav.dart';
+import 'package:travel/widget/webview.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 
@@ -17,20 +22,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List _imageUrls = [
-    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
-    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
-    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
-  ];
-
   double appBarAlpha = 0;
   List<CommonModel> localNavList = [];
+  List<CommonModel> bannerList = [];
   GridNavModel gridNavModel;
+  List<CommonModel> subNavList = [];
+  SalesBoxModel salesBoxModel;
+  bool _loading = true;
 
   @override
   void initState() {
     super.initState();
-    loadData();
+    _handleRefresh();
   }
 
   _onScroll(offset) {
@@ -46,7 +49,7 @@ class _HomePageState extends State<HomePage> {
     print(appBarAlpha);
   }
 
-  loadData() async {
+  Future<Null> _handleRefresh() async {
     //网络请求方法一
 //    HomeDao.fetch().then((result) {
 //      setState(() {
@@ -57,90 +60,130 @@ class _HomePageState extends State<HomePage> {
 //        resultString = e.toString();
 //      });
 //    });
-
     //网络请求方法二
     try {
       HomeModel model = await HomeDao.fetch();
       setState(() {
         localNavList = model.localNavList;
+        bannerList = model.bannerList;
         gridNavModel = model.gridNav;
+        subNavList = model.subNavList;
+        salesBoxModel = model.salesBox;
+        _loading = false;
       });
     } catch (e) {
+      print(e);
       setState(() {
-        print(e);
+        _loading = false;
       });
     }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0Xfff2f2f2),
-      body: Stack(
-        children: [
-          MediaQuery.removePadding(
-            removeTop: true,
-            context: context,
-            child: NotificationListener(
-              onNotification: (scrollNotification) {
-                if (scrollNotification is ScrollUpdateNotification &&
-                    scrollNotification.depth == 0) {
-                  //该方法会监听所有的滚动，包括列表里面的banner的滚动，需要过滤如下：
-                  //1.滚动且是列表滚动的时候(刚进入的时候也会回调，所以需要加此判断)
-                  //2.scrollNotification.depth == 0 表示 scrollNotification 的深度是0，即第0个元素，也就是child中的第一个View ListView 滚动的时候
-                  //再执行下面的方法。
-                  _onScroll(scrollNotification.metrics.pixels);
-                }
-                return false;
-              },
-              child: ListView(
-                children: [
-                  Container(
-                    height: 160,
-                    child: Swiper(
-                      itemCount: _imageUrls.length,
-                      autoplay: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Image.network(
-                          _imageUrls[index],
-                          fit: BoxFit.fill,
-                        );
-                      },
-                      pagination: SwiperPagination(),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                    child: LocalNav(localNavList: localNavList),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                    child: GridNav(gridNavModel: gridNavModel),
-                  ),
-                  Container(
-                    height: 800,
-                    child: ListTile(
-                      title: Text('resultString'),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Opacity(
-            opacity: appBarAlpha,
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text('首页'),
+      body: LoadingContainer(
+        isLoading: _loading,
+        child: Stack(
+          children: [
+            MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: RefreshIndicator(
+                onRefresh: _handleRefresh,
+                child: NotificationListener(
+                  onNotification: (scrollNotification) {
+                    if (scrollNotification is ScrollUpdateNotification &&
+                        scrollNotification.depth == 0) {
+                      //该方法会监听所有的滚动，包括列表里面的banner的滚动，需要过滤如下：
+                      //1.滚动且是列表滚动的时候(刚进入的时候也会回调，所以需要加此判断)
+                      //2.scrollNotification.depth == 0 表示 scrollNotification 的深度是0，即第0个元素，也就是child中的第一个View ListView 滚动的时候
+                      //再执行下面的方法。
+                      _onScroll(scrollNotification.metrics.pixels);
+                    }
+                    return false;
+                  },
+                  child: _listView,
                 ),
               ),
             ),
-          )
-        ],
+            _appBar,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget get _listView {
+    return ListView(
+      children: [
+        _banner,
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+          child: LocalNav(localNavList: localNavList),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+          child: GridNav(gridNavModel: gridNavModel),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+          child: SubNav(subNavList: subNavList),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+          child: SalesBox(salesBox: salesBoxModel),
+        ),
+      ],
+    );
+  }
+
+  Widget get _appBar {
+    return Opacity(
+      opacity: appBarAlpha,
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text('首页'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget get _banner {
+    return Container(
+      height: 160,
+      child: Swiper(
+        itemCount: bannerList.length,
+        autoplay: true,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  CommonModel model = bannerList[index];
+                  return WebView(
+                    url: model.url,
+                    statusBarColor: model.statusBarColor,
+                    hideAppBar: model.hideAppBar,
+                  );
+                }),
+              );
+            },
+            child: Image.network(
+              bannerList[index].icon,
+              fit: BoxFit.fill,
+            ),
+          );
+        },
+        pagination: SwiperPagination(),
       ),
     );
   }
